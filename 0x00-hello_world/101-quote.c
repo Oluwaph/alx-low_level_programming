d1fa826864752ae22c771ee79e007463269cbd34
Peter David <peteroludavid11@gmail.com>
2023-02-16 19:20:26 -0800

    Unix simplicity

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/* main - starting point for program execution
+ *
+ * Return: end point of a program execution, return 1 (success)
+ */
+
+int main(void)
+{
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	return(1);
+}
