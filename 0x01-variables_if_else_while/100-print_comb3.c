97db9aa51dbfdd796947e1a70a4ba41a13bd8dec
Peter David <peteroludavid11@gmail.com>
2023-02-19 16:39:49 -0800

    possible combinations of 2digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - starting point of program execution
+ * prints all possible different combinations of two digits
+ *
+ * Return: end point of program execution
+ */
+int main(void)
+{
+	int n, m;
+
+	for (m = 48; m <= 56; m++)
+	{
+		for (m = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
