eff92ee5cde0678cfc8ba89fee293b616244936a
Peter David <peteroludavid11@gmail.com>
2023-02-19 17:08:35 -0800

    possible combinations of 3digits

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - beginning of program execution
+ * prints all possible different combinations of three digits
+ *
+ * Return: the end of program execution
+ */
+int main(void)
+{
+	int n, m, l;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		for (m = '0'; m <= '9'; m++)
+		{
+			for (l = '0'; l <= '9'; l++)
+			{
+				if (n < m && m < l)
+				{
+					putchar(n);
+					putchar(m);
+					putchar(l);
+
+					if (c != '7')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
