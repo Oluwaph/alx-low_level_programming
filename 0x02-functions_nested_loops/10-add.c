c16c37af27d00112e28c4ebbd03c8f4bf762e8b3
Peter David <peteroludavid11@gmail.com>
2023-02-22 16:06:19 -0800

    program to add 2 numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - begining of the program to add two numbers
+ *
+ * @x: 1st declared variable
+ * @y: 2nd declared variable
+ *
+ * Return: add result
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
