9cf26e2bd1a5587d0f8d1520ea72671f86829774
Peter David <peteroludavid11@gmail.com>
2023-02-23 18:24:44 -0800

    largest of 3 integers

---
+++ 0x03-debugging/2-largest_number.c
@@ -0,0 +1,33 @@
+#include "mainh.h"
+
+/**
+ * largest_number - prints the largest of three integers
+ * @a: first integer
+ * @b: second integer
+ * @c: thrid integer
+ * Return: largest integer
+ */
+
+int largest_number(int a, int b, int c)
+{
+	int largest;
+
+	if (a > b && a > c)
+	{
+		largest = a;
+	}
+	else if (a > b && c > a)
+	{
+		largest = c;
+	}
+	else if (b > c)
+	{
+		largest = b;
+	}
+	else
+	{
+		largest = c;
+	}
+
+	return (largest);
+}
