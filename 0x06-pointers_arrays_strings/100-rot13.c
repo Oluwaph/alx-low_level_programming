4bec5f427aec1d977781ce485622dd87044c203b
Peter David <peteroludavid11@gmail.com>
2023-03-02 15:01:32 -0800

    function that encodes a string using rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -15,7 +15,7 @@ char *rot13(char *s)
 
 	char datarot[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 
-	for (i = 0; s[i] != '\0'; 1++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; j < 52; j++)
 		{
